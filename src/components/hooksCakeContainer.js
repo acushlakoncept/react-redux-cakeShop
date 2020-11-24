8483a2df6b654a6eba373e9b468ee10afa495a41
Uduak Essien <acushla4real@gmail.com>
2020-11-24 17:50:27 +0100

    add cakesContainer using hooks

---
+++ src/components/hooksCakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hookscakecontainer() {
+  return (
+    <div>
+      <h2>Num of cakes - </h2>
+      <button>Buy cake</button>
+    </div>
+  );
+}
+
+export default Hookscakecontainer;
