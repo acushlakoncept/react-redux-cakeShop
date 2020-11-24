5d3b83c6e8495813dd69fcbbfa60b4aa33de00b5
Uduak Essien <acushla4real@gmail.com>
2020-11-24 15:38:47 +0100

    add cakeContainer.js

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Cakecontainer() {
+    
+
+    return (
+        <div>
+            <h2>Number of Cakes</h2>
+            <button>Buy cakes</button>
+        </div>
+    )
+}
