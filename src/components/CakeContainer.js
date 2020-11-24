985b56c161e6382bfddf2d54dc3b8380b591b7d0
Uduak Essien <acushla4real@gmail.com>
2020-11-24 16:00:10 +0100

    add mapStateToProps and DispatchToProps actions using connect

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
+import { buyCake } from '../redux'
+import { connect } from 'react-redux'
 
-export default function Cakecontainer() {
-    
-
-    return (
-        <div>
-            <h2>Number of Cakes</h2>
-            <button>Buy cakes</button>
-        </div>
-    )
+function Cakecontainer(props) {
+  return (
+    <div>
+      <h2>Number of cakes - {props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy cakes</button>
+    </div>
+  );
 }
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cakecontainer)
