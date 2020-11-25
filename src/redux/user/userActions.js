7085c2b80209698531860a78cb5a87e34c74e96e
Uduak Essien <acushla4real@gmail.com>
2020-11-25 04:27:54 +0100

    add fetchUsers method using axios

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,21 +1,41 @@
-import { FETCH_USERS_FAILURE, FETCH_USERS_SUCCESS, FETCH_USERS_REQUEST} from './userTypes'
+import axios from "axios";
+import {
+  FETCH_USERS_FAILURE,
+  FETCH_USERS_SUCCESS,
+  FETCH_USERS_REQUEST
+} from "./userTypes";
 
 export const fetchUserRequest = () => {
-    return {
-        type: FETCH_USERS_REQUEST
-    }
-}
+  return {
+    type: FETCH_USERS_REQUEST
+  };
+};
 
-export const fetchUserSuccess = users => {
-    return {
-        type: FETCH_USERS_SUCCESS,
-        payload: users
-    }
-}
+export const fetchUserSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  };
+};
 
-export const fetchUserFailure = error => {
-    return {
-        type: FETCH_USERS_FAILURE,
-        payload: error
-    }
-}
+export const fetchUserFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error
+  };
+};
+
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUserRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUserSuccess(users));
+      })
+      .catch((error) => {
+        dispatch(fetchUserFailure(error.message));
+      });
+  };
+};
